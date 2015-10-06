d31e1250b719ce7249a5b3ad3f0dfcd61061d49d
alexax66 <alexax66@gmail.com>
2015-10-06 15:03:42 +0000

    build_kernel.sh: use Linaro GCC 4.7.4-2014.06 Toolchain

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 export ARCH=arm
-export CROSS_COMPILE=/opt/toolchains/arm-eabi-4.7/bin/arm-eabi-
+export CROSS_COMPILE=/opt/toolchains/arm-cortex_a15-linux-gnueabihf-linaro_4.7.4-2014.06/bin/arm-eabi-
 mkdir output
 
 make -C $(pwd) O=output VARIANT_DEFCONFIG=msm8916_sec_e53g_eur_defconfig msm8916_sec_defconfig SELINUX_DEFCONFIG=selinux_defconfig
-make -C $(pwd) O=output
+make -j5 -C $(pwd) O=output
 
 cp output/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
