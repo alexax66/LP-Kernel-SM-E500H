f9b1a817b6f30704e320ddb93b8e86a01c999763
alexax66 <alexax66@gmail.com>
2015-10-06 19:59:46 +0000

    build_kernel.sh: add choice for SM-E500F variant

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 
+case "$1" in
+         H)
+            VARIANT="e53g"
+            ;;
+
+         F)
+            VARIANT="e5"
+            ;;
+
+         *)
+            VARIANT="e53g"
+esac
+
+rm -rf $(pwd)/output/arch/arm/boot/dts/*
+rm -f $(pwd)/output/arch/arm/boot/dt.img
+
 export ARCH=arm
 export CROSS_COMPILE=/opt/toolchains/arm-cortex_a15-linux-gnueabihf-linaro_4.7.4-2014.06/bin/arm-eabi-
 mkdir output
 
-make -C $(pwd) O=output VARIANT_DEFCONFIG=msm8916_sec_e53g_eur_defconfig msm8916_sec_defconfig SELINUX_DEFCONFIG=selinux_defconfig
+make -C $(pwd) O=output VARIANT_DEFCONFIG=msm8916_sec_$VARIANT"_eur_defconfig" msm8916_sec_defconfig SELINUX_DEFCONFIG=selinux_defconfig
 make -j5 -C $(pwd) O=output
 
 $(pwd)/dtbTool -2 -o $(pwd)/output/arch/arm/boot/dt.img -s 2048 -p $(pwd)/output/scripts/dtc/ $(pwd)/output/arch/arm/boot/dts/ -v
-cp output/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
+cp output/arch/arm/boot/zImage $(pwd)/arch/arm/boot/zImage
