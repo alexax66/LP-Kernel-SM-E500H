2220ab21e5bb75117379048f5d6b2fb53d77046e
alexax66 <alexax66@gmail.com>
2015-10-06 15:30:50 +0000

    Add tools for making dt.img

--- build_kernel.sh
+++ build_kernel.sh
@@ -7,4 +7,5 @@ mkdir output
 make -C $(pwd) O=output VARIANT_DEFCONFIG=msm8916_sec_e53g_eur_defconfig msm8916_sec_defconfig SELINUX_DEFCONFIG=selinux_defconfig
 make -j5 -C $(pwd) O=output
 
+$(pwd)/dtbTool -2 -o $(pwd)/output/arch/arm/boot/dt.img -s 2048 -p $(pwd)/output/scripts/dtc/ $(pwd)/output/arch/arm/boot/dts/ -v
 cp output/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
